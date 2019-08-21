4d08ab0c9f2bc9e2843cd668cfd7631ef70536fa
linhtv97 <linh.cntt097@gmail.com>
2019-08-21 15:37:10 +0700

    first commit

--- src/http/HelloController.js
+++ src/http/HelloController.js
@@ -1,10 +1,12 @@
-import { singleton, get, Controller } from "@fusion.io/proton";
+import { singleton, get } from "@fusion.io/proton";
 
 @singleton()
-export default class HelloController extends Controller {
+export default class HelloController {
 
     @get('/')
     async index(context) {
-        context.render('hello', { message: "tokamak.fuse(ProtonPlasma);" });
+       context.body = {
+           message: "foo-baz"
+       }
     }
 }
