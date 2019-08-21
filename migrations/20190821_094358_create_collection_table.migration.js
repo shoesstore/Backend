d19ee1b503ec158c0901357a808432d12a88056c
huong9x <huongnv@sphinx-software.com>
2019-08-21 16:50:19 +0700

    add collection controller

---
+++ migrations/20190821_094358_create_collection_table.migration.js
@@ -0,0 +1,18 @@
+export default class CreateCollectionTable {
+
+    async up(schema) {
+        await schema.createTable('collections', table => {
+            table.increments();
+            table.integer('parent_id');
+            table.string('name');
+            table.string('slug');
+            table.specificType('related_slugs', 'text ARRAY');
+            table.timestamps();
+            table.timestamp('deleted_at');
+        });
+    }
+
+    async down(schema) {
+        await schema.dropTable('collections');
+    }
+}
