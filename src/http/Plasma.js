d2d8fd24d64da34404ad9b8ddd7f3c0028c6adad
huong9x <huongnv@sphinx-software.com>
2019-08-23 10:23:04 +0700

    collection done

--- src/http/Plasma.js
+++ src/http/Plasma.js
@@ -1,15 +1,17 @@
 import {Plasma as CorePlasma, inject, Kernel, Router} from "@fusion.io/proton";
-import HelloController from "./HelloController";
+import HelloController      from "./HelloController";
+import CollectionController from "./CollectionController";
+import BodyParser           from "koa-bodyparser";
 
 export default class Plasma extends CorePlasma {
-
     @inject(Kernel, Router)
     boot(kernel, router) {
 
-
+        kernel.use(BodyParser());
         kernel.use(router.routes());
         kernel.use(router.allowedMethods());
-
         router.controller(HelloController);
+        router.controller(CollectionController);
+
     }
 }
